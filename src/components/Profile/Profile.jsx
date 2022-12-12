50e512e721e49df1a2c4c9fc2038459a7f6c8865
VitaliiTymko <intehmet2015@gmail.com>
2022-12-13 01:40:49 +0200

    add user

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,5 @@
+import  ReactDOM  from "react";
+
+const jsxElement = <div>Hello gyse</div>;
+console.log(jsxElement);
+console.log(555);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,60 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// ====================================================
+
+const data = {
+  username: 'Jacques Gluke',
+  tag: 'jgluke',
+  location: 'Ocho Rios, Jamaica',
+  avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+console.log(data);
+
+const painting = (
+  <div class="profile">
+    <div class="description">
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+        alt="User avatar"
+        class="avatar"
+      />
+      <p class="name">Petra Marica</p>
+      <p class="tag">@pmarica</p>
+      <p class="location">Salvador, Brasil</p>
+    </div>
+
+    <ul class="stats">
+      <li>
+        <span class="label">Followers</span>
+        <span class="quantity">1000</span>
+      </li>
+      <li>
+        <span class="label">Views</span>
+        <span class="quantity">2000</span>
+      </li>
+      <li>
+        <span class="label">Likes</span>
+        <span class="quantity">3000</span>
+      </li>
+    </ul>
+  </div>
+);
+
+
+console.log(painting);
+
+// ReactDOM.render(painting, document.querySelector('#root'));
