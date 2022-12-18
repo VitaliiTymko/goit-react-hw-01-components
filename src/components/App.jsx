e9bc6ff736d92021de59d92103e457fd19193def
VitaliiTymko <intehmet2015@gmail.com>
2022-12-19 00:19:50 +0200

    Transactions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import data from './Statistics/data.json';
 import Statistics from './Statistics/Statistics';
 import friends from './Friends/friends.json';
 import FriendList from './Friends/FriendList';
+import transactions from './Transactions/transactions.json';
+import TransactionHistory from './Transactions/TransactionHistory';
 
 export const App = () => {
   return (
@@ -29,6 +31,7 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
